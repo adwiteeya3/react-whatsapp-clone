cbb9b2ab448463b889fc3ec3b451146fd602f83e
Adwiteeya <adwiteeya3@gmail.com>
2020-08-19 22:18:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <h1>Let's build whatsapp</h1>
-
       <div className='app_body'>
         {/* Sidebar */}
         {/* Chat */}
---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sidebar.css';
+
+function Sidebar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Sidebar
