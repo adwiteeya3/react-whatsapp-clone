c0461be5358d03fb8d126af9af4adc9099998864
Adwiteeya <adwiteeya3@gmail.com>
2020-08-19 22:31:49 +0530

    sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './Sidebar.css';
+import { Avatar } from '@material-ui/core';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function Sidebar() {
     return (
         <div className='sidebar'>
-            <h1>Sidebar</h1> 
             <div className='sidebar_header'>
-
+            <Avatar />
+                <div className='sidebar_headerRight'>
+                    <DonutLargeIcon />
+                    <ChatIcon />
+                    <MoreVertIcon />
+                </div>
             </div>
 
             <div className='sidebar_search'>
