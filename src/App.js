0f927b3cc691815d6600614652af7fd3a1f413a9
Adwiteeya <adwiteeya3@gmail.com>
2020-08-19 22:23:02 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
       <div className='app_body'>
         {/* Sidebar */}
+        <Sidebar />
         {/* Chat */}
       </div>
     </div>
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,10 +3,21 @@ import './Sidebar.css';
 
 function Sidebar() {
     return (
-        <div>
-            
+        <div className='sidebar'>
+            <h1>Sidebar</h1> 
+            <div className='sidebar_header'>
+
+            </div>
+
+            <div className='sidebar_search'>
+
+            </div>
+
+            <div className='sidebar_chats'>
+                
+            </div>
         </div>
-    )
+    );
 }
 
-export default Sidebar
+export default Sidebar;
