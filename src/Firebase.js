79798c7aa4ffd204c2cfcde1d5d842e062d2fbad
Adwiteeya <adwiteeya3@gmail.com>
2020-08-19 22:01:44 +0530

    installing react and firebase

---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD10o4uDELueyGSQWl6LknzewJZ3QaQCVg",
+    authDomain: "whatsapp-clone-3e053.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-3e053.firebaseio.com",
+    projectId: "whatsapp-clone-3e053",
+    storageBucket: "whatsapp-clone-3e053.appspot.com",
+    messagingSenderId: "498855640575",
+    appId: "1:498855640575:web:ecc535c51816d4d3077bca",
+    measurementId: "G-1DE3051MT4"
+  };
