9520b3243ffe11e80c403c0fc5f275e7dcfe4650
Adwiteeya <adwiteeya3@gmail.com>
2020-08-19 22:14:17 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Let's build whatsapp</h1>
+
+      <div className='app_body'>
+        {/* Sidebar */}
+        {/* Chat */}
+      </div>
     </div>
   );
 }
