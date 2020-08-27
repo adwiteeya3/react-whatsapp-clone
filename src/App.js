65c2d3a6529b8ef83b7b31f5b9a52f06555152b6
Adwiteeya <adwiteeya3@gmail.com>
2020-08-27 22:08:18 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
       <div className='app_body'>
         <Router>
-        <Switch>
-          
-          <Sidebar />   {/* Sidebar */}
-          <Chat />      {/* Chat */}
-          
-        </Switch>
+          <Switch>
+            <Sidebar />
+            <Route path='/rooms/:roomId'>
+              <Chat />
+            </Route>
+
+            <Route path='/'>
+              <Chat />
+            </Route>
+          </Switch>
         </Router >
       </div>
     </div>
