88d1f033b4e0747161f0260e4904363df4e4e13a
Adwiteeya <adwiteeya3@gmail.com>
2020-08-27 23:45:08 +0530

    update

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useStateValue } from 'react';
 import "./Login.css";
 import { Button } from '@material-ui/core';
 import { auth, provider } from './firebase';
+import { actionTypes } from './reducer';
 
 function Login() {
+    const [{}, dispatch] = useStateValue();
+
     const signIn = () => {
         auth
             .signInWithPopup(provider)
-            .then((result) => console.log(result));
+            .then((result) => {
+                dispatch({
+                    type: actionTypes.SET_USER,
+                    user: result.user,
+                });
+            })
+            .catch(console.error);
     };
 
     return (
