b04733942dcc9a82e4346f4f05c475c57548b2d1
Adwiteeya <adwiteeya3@gmail.com>
2020-08-27 15:41:54 +0530

    chat

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
+import Chat from './Chat';
 
 function App() {
   return (
---
+++ src/Chat.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Chat.css';
+import { Avatar } from '@material-ui/core';
+
+function Chat() {
+    return (
+        <div className='chat'>
+            <div className='chat_header'>
+                <Avatar />
+            </div>
+
+            <div className='chat_body'>
+
+            </div>
+
+            <div className="chat_footer">
+
+            </div>
+        </div>
+    )
+}
+
+export default Chat; 
