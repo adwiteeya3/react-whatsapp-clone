40245c8f8f493f01d4af94399e66ebb0fb4c390d
Adwiteeya <adwiteeya3@gmail.com>
2020-08-27 23:15:20 +0530

    login

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./Login.css";
 import { Button } from '@material-ui/core';
-import { auth, provider } from 'firebase';
+import { auth, provider } from './firebase';
 
 function Login() {
     const signIn = () => {
-        auth.signInWithPopup(provider)
+        auth
+            .signInWithPopup(provider)
+            .then((result) => console.log(result));
     };
 
     return (
