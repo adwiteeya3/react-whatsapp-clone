587803e354696172270efab171ca75c4b0202f6a
Adwiteeya <adwiteeya3@gmail.com>
2020-08-28 00:39:03 +0530

    commit

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -6,6 +6,20 @@ import { Link } from 'react-router-dom';
 
 function SidebarChat({ id, name, addNewChat }) {
     const [seed, setSeed] = useState("");
+    const [messages, setMessages] = useState("");
+
+    useEffect(() => {
+        if (id) {
+            db.collection('rooms')
+            .doc(id)
+            .collection('messages')
+            .orderBy('timestamp', 'desc')
+            .onSnapshot((snapshot) =>
+                setMessages(snapshot.docs.map((doc) => 
+                doc.data()))
+            );
+        }
+    }, [id]);
     
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000));
@@ -26,7 +40,7 @@ function SidebarChat({ id, name, addNewChat }) {
             <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
             <div className='sidebarChat_info'>
                 <h2>{name}</h2>
-                <p>Last message...</p>
+                <p>{messages[0]?.message}</p>
             </div>
         </div>
         </Link>
